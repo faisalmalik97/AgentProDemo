243cd37255fd3b45b65823b4eca88453591ac019
Faisal Malik <74533041+faisalmalik97@users.noreply.github.com>
2025-06-05 17:27:25 +0800

    Update app.py
    resopnse added at end

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ agent = ReactAgent(model=model, tools=tools)
 #query = "capital city of australia?"
 response = agent.run(query)
 
-return response
+
 
 def main():
     col1, col2 = st.columns([1,5])
@@ -68,5 +68,5 @@ def main():
         run_button = st.button("Run Agent", use_container_width=True)
         
         
-
+return response
 #print(f"\nFinal Answer: {response.final_answer}")
