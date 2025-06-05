9aece47eefdb25ee3806ad8e4b1831e56f81d2c3
Faisal Malik <74533041+faisalmalik97@users.noreply.github.com>
2025-06-05 14:30:13 +0800

    Update app.py
    BODY ADDED

--- app.py
+++ app.py
@@ -13,3 +13,21 @@ from openai import OpenAI
 st.set_page_config(page_title="Support Assistant Agent", layout="wide")
 st.title("🔍 Support Assistan Agent")
 st.header("Header of AI Agent")
+
+
+with st.expander("Example query (read-only)"):
+    st.text_area("Sample Query", value=sample_query, height=200, disabled=True)
+
+input_mode = st.radio("Choose input mode:", ["Enter full query text", "Set parameters"])
+
+if input_mode == "Enter full query text":
+    user_query = st.text_area("Enter your full query here", height=250)
+    analyze_btn = st.sidebar.button("Run AGENT")
+else:
+    # Sidebar parameters
+    st.sidebar.header("Signal Simulation Parameters")
+    duration = st.sidebar.slider("Duration (s)", 1, 10, 2)
+    sampling_rate = st.sidebar.slider("Sampling Rate (Hz)", 500, 10000, 2000)
+    frequencies = st.sidebar.text_input("Frequencies (comma-separated)", "50,150,300")
+    amplitudes = st.sidebar.text_input("Amplitudes (comma-separated)", "1,0.5,0.2")
+    analyze_btn = st.sidebar.button("Run AGENT")
