3903e1ef6b9947581c53a0999447818805d038fb
Faisal Malik <74533041+faisalmalik97@users.noreply.github.com>
2025-06-05 17:22:04 +0800

    Update app.py
    added button

--- app.py
+++ app.py
@@ -6,23 +6,25 @@ from agentpro import create_model
 from openai import OpenAI
 
 
-# Set your API keys here or use Streamlit secrets
+# API keys from Streamlit secrets
 api_key = st.secrets["OPENAI_API_KEY"]
 ares_key = st.secrets["ARES_API_KEY"]
 
-st.set_page_config(page_title="Support Assistant Agent", layout="wide")
+st.set_page_config(
+page_title="Support Assistant Agent", layout="wide"
+)
 st.title("🔍 Support Assistan Agent")
 st.header("Header of AI Agent")
 
 # Example query 
-sample_query = """
-THIS IS AN EXAMPLE QUERY TO TEST AREA OF INPUT
-"""
+#sample_query = """
+#THIS IS AN EXAMPLE QUERY TO TEST AREA OF INPUT
+#"""
 
-with st.expander("Example query (read-only)"):
-    st.text_area("Sample Query", value=sample_query, height=200, disabled=True)
+#with st.expander("Example query (read-only)"):
+ #   st.text_area("Sample Query", value=sample_query, height=200, disabled=True)
 
-input_mode = st.radio("Choose input mode:", ["Enter full query text", "Set parameters"])
+#input_mode = st.radio("Choose input mode:", ["Enter full query text", "Set parameters"])
 
 
 # Create a model with OpenAI
@@ -41,5 +43,30 @@ response = agent.run(query)
 
 return response
 
+def main()
+    col1, col2 = st.columns([1,5])
+    with col1:
+        st.markdown(" Hi")
+    with col2:
+        st.markdown(" Hi Again")
+
+    st.markdown(" Processing ")
+
+    with st.container():
+        st.markdown("""
+        <div style="background-color: white">
+        <h3> Input your query here </h3>
+        </div>
+        """ , unsafe_allow_html=True)
+
+#Input section
+    query = st.text_area("", height=100, placeholder="Smaple question here")
+
+#Button area
+    coll, col2, col3 = st. columns ([1, 1, 1])
+    with col2:
+        run_button = st.button("Run Agent", use_container_width=True)
+        
+        
 
 #print(f"\nFinal Answer: {response.final_answer}")
