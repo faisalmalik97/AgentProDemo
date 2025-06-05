c2b7c461bbfb99392b076a9e7bd471137f89da7f
Faisal Malik <74533041+faisalmalik97@users.noreply.github.com>
2025-06-05 14:10:22 +0800

    Create app.py
    created app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import openai
+from agentpro import create_model, ReactAgent
+from agentpro.tools import AresInternetTool, UserInputTool, CalculateTool , SignalGeneratorAgent, SignalAnalyzerAgent, SignalDiagnosticAgent, ReportAgent  
+
+# Set your API keys here or use Streamlit secrets
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+ares_key = st.secrets["ARES_KEY"]
+
+st.set_page_config(page_title="Agent Assistant", layout="wide")
+st.title("🔍 AI Agent")
+st.header("AI Agent")
