4853822320f514fd38c295a1475da52e2fd3a468
Faisal Malik <74533041+faisalmalik97@users.noreply.github.com>
2025-06-05 19:55:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,8 @@ with st.expander("You can ask questions like..... (click here ! )"):
 #Query input area  
 user_query = st.text_area("Enter your question here ⬇️ ", height=250)
 
-#answer_btn = st.sidebar.button("ANSWER 🔲 ", use_container_width=True)
-answer_btn = st.button("ANSWER 🔲 ", use_container_width=True)
+answer_btn = st.sidebar.button("ANSWER 🔲 ", use_container_width=True)
+#answer_btn = st.button("ANSWER 🔲 ", use_container_width=True)
 
 if answer_btn:
   time.sleep(2)
